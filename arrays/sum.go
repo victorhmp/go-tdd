365874e844d52b4f41b53d9f345926abba0fd01f
Victor Hugo M. Pinto <victor2142@gmail.com>
2024-06-23 19:06:10 -0300

    Use slices instead of fixed length arrays in arrays.Sum

--- arrays/sum.go
+++ arrays/sum.go
@@ -4,7 +4,7 @@ package main
 // fixed to its type declaration. So a function that expects [5]int will NOT
 // accept an array that is not of length 5.
 
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
 
 	// Notice that range is returning (index, value)
@@ -14,3 +14,19 @@ func Sum(numbers [5]int) int {
 
 	return sum
 }
+
+// From the docs:
+// Arrays are useful when planning the detailed layout of memory and sometimes can help avoid allocation [...]
+//
+// There are major differences between the ways arrays work in Go and C. In Go,
+//
+// - Arrays are values. Assigning one array to another copies all the elements.
+// - In particular, if you pass an array to a function, it will receive a copy of the array, not a pointer to it.
+// - The size of an array is part of its type. The types [10]int and [20]int are distinct.
+
+// About Slices:
+// Slices wrap arrays to give a more general, powerful, and convenient interface
+// to sequences of data. Except for items with explicit dimension such as
+// transformation matrices, most array programming in Go is done with slices
+// rather than simple arrays.
+// Important to note that slices are **references** to an underlying array.
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -3,13 +3,25 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("Empty collection", func(t *testing.T) {
+		numbers := []int{}
 
-	got := Sum(numbers)
-	expected := 15
+		got := Sum(numbers)
+		expected := 0
 
-	if got != expected {
-		t.Errorf("got %d expected %d input: %v", got, expected, numbers)
-	}
+		if got != expected {
+			t.Errorf("got %d expected %d input: %v", got, expected, numbers)
+		}
+	})
 
+	t.Run("Collection of n items", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+
+		got := Sum(numbers)
+		expected := 15
+
+		if got != expected {
+			t.Errorf("got %d expected %d input: %v", got, expected, numbers)
+		}
+	})
 }

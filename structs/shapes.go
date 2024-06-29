f32e8d17fe01a59024a109c7a51587424960825f
Victor Hugo M. Pinto <victor2142@gmail.com>
2024-06-29 17:09:03 -0300

    Add a new struct that implements the Shape interface, along with testing

--- structs/shapes.go
+++ structs/shapes.go
@@ -21,6 +21,11 @@ type Circle struct {
 	Radius float64
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
 func Perimeter(rectangle Rectangle) float64 {
 	return 2 * (rectangle.Width + rectangle.Height)
 }
@@ -31,4 +36,8 @@ func (r Rectangle) Area() float64 {
 
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
+}
+
+func (t Triangle) Area() float64 {
+	return 0.5 * t.Base * t.Height
 }

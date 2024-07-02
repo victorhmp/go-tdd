b5396c1c0c36bc4144135593ee09fa61cff7e46e
Victor Hugo M. Pinto <victor2142@gmail.com>
2024-07-01 22:48:56 -0300

    Add error handling for Search when dealing with unknown words

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,7 +1,19 @@
 package maps
 
+import "errors"
+
 type Dictionary map[string]string
 
-func (d Dictionary) Search(word string) string {
-	return d[word]
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
+func (d Dictionary) Search(word string) (string, error) {
+	// The second return value of a map access is a boolean value that reports
+	// whether the requested key was present in the map or not.
+	definition, ok := d[word]
+
+	if !ok {
+		return "", ErrNotFound
+	}
+
+	return definition, nil
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -3,12 +3,26 @@ package maps
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dictionary := Dictionary{"test": "this is just a test"}
+	t.Run("known word", func(t *testing.T) {
+		dictionary := Dictionary{"test": "this is just a test"}
 
-	got := dictionary.Search("test")
-	expected := "this is just a test"
+		got, _ := dictionary.Search("test")
+		expected := "this is just a test"
 
-	assertStrings(t, got, expected)
+		assertStrings(t, got, expected)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		dictionary := Dictionary{"test": "this is just a test"}
+
+		_, err := dictionary.Search("unknown")
+
+		if err == nil {
+			t.Fatal("expected to get an error.")
+		}
+
+		assertError(t, err, ErrNotFound)
+	})
 }
 
 func assertStrings(t testing.TB, got, expected string) {
@@ -16,5 +30,13 @@ func assertStrings(t testing.TB, got, expected string) {
 
 	if got != expected {
 		t.Errorf("got %q want %q", got, expected)
+	}
+}
+
+func assertError(t testing.TB, got, expected error) {
+	t.Helper()
+
+	if got != expected {
+		t.Errorf("got %q, expected %q", got, expected)
 	}
 }

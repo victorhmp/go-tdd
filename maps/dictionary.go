349862e3ab5ecb42c292e2a7a52d82abd092e995
Victor Hugo M. Pinto <victor2142@gmail.com>
2024-07-01 23:30:24 -0300

    Add Add method to Dictionary and handle errors

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,10 +1,16 @@
 package maps
 
-import "errors"
-
 type Dictionary map[string]string
+type DictionaryErr string
 
-var ErrNotFound = errors.New("could not find the word you were looking for")
+const (
+	ErrNotFound   = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists = DictionaryErr("word already exists in the dictionary")
+)
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
 
 func (d Dictionary) Search(word string) (string, error) {
 	// The second return value of a map access is a boolean value that reports
@@ -17,3 +23,27 @@ func (d Dictionary) Search(word string) (string, error) {
 
 	return definition, nil
 }
+
+func (d Dictionary) Add(word, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
+}
+
+// Important to note about Maps: they are NOT reference types. A map value is a
+// reference to a runtime hash table. So when you pass a map to a function, you
+// are copying it, but just the **pointer**, not the underlying data.
+// Also, note that maps can be `nil`. You can read from a `nil` map, but you
+// can't write to it. Because of this "interesting feature", it's a good idea
+// to **never** initialize a `nil` map and always use `make` to create a new map
+// or explicitly create an empty map. Both of these are still ideomatic Go and
+// and actually create empty hash maps, ensuring you don't get runtime panics.
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -25,6 +25,42 @@ func TestSearch(t *testing.T) {
 	})
 }
 
+func TestAddWord(t *testing.T) {
+	t.Run("new word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		newWord := "test"
+		newDefinition := "this is just a test"
+
+		err := dictionary.Add(newWord, newDefinition)
+
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, newWord, newDefinition)
+	})
+
+	t.Run("existing word", func(t *testing.T) {
+		newWord := "test"
+		newDefinition := "this is just a test"
+		dictionary := Dictionary{newWord: newDefinition}
+
+		err := dictionary.Add(newWord, newDefinition)
+
+		assertError(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, newWord, newDefinition)
+	})
+}
+
+func assertDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
+	t.Helper()
+
+	got, err := dictionary.Search(word)
+
+	if err != nil {
+		t.Fatal("should have found the word:", word)
+	}
+
+	assertStrings(t, got, definition)
+}
+
 func assertStrings(t testing.TB, got, expected string) {
 	t.Helper()
 

82f919dbd716aaed8a565bc6e9a47b03409c2c66
Victor Hugo M. Pinto <victor2142@gmail.com>
2024-07-04 22:58:05 -0300

    Refactor tests for Countdown to use a more flexible Spy and validate function call order

--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -2,34 +2,64 @@ package main
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
 
-type SpySleeper struct {
-	Calls int
+const sleepOperation = "sleep"
+const writeOperation = "write"
+
+type SpyCountdownOperations struct {
+	Calls []string
 }
 
-func (s *SpySleeper) Sleep() {
-	s.Calls++
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleepOperation)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, writeOperation)
+
+	return
 }
 
 func TestCountdown(t *testing.T) {
-	buffer := &bytes.Buffer{}
-	spySleeper := &SpySleeper{}
+	t.Run("prints a countdown from 3, ending in 'Go!'", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		spySleepWrite := &SpyCountdownOperations{}
 
-	Countdown(buffer, spySleeper)
+		Countdown(buffer, spySleepWrite)
 
-	got := buffer.String()
-	expected := `3
+		got := buffer.String()
+		expected := `3
 2
 1
 Go!`
 
-	if got != expected {
-		t.Errorf("got %q expected %q", got, expected)
-	}
+		if got != expected {
+			t.Errorf("got %q expected %q", got, expected)
+		}
+	})
 
-	if spySleeper.Calls != 3 {
-		t.Errorf("Not enough calls to sleeper. Expected 3 got %d", spySleeper.Calls)
-	}
+	t.Run("sleep is called before every print", func(t *testing.T) {
+		spyOperations := &SpyCountdownOperations{}
+
+		// Since our SpyCountdownOperations implements both io.Writer and Sleeper,
+		// we can pass it to both arguments Countdown expects.
+		Countdown(spyOperations, spyOperations)
+
+		expected := []string{
+			writeOperation,
+			sleepOperation,
+			writeOperation,
+			sleepOperation,
+			writeOperation,
+			sleepOperation,
+			writeOperation,
+		}
+
+		if !reflect.DeepEqual(expected, spyOperations.Calls) {
+			t.Errorf("Expected calls %v got %v", expected, spyOperations.Calls)
+		}
+	})
 }

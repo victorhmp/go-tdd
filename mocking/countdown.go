299af4cb1b4cb2423467af331bdaebade7d8ce3f
Victor Hugo M. Pinto <victor2142@gmail.com>
2024-07-04 22:36:46 -0300

    Add support for dependency injection and use it to mock sleeper function

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -7,19 +7,30 @@ import (
 	"time"
 )
 
+type Sleeper interface {
+	Sleep()
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
 const finalWord = "Go!"
 const countdownStart = 3
 
-func Countdown(output io.Writer) {
+func Countdown(output io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(output, i)
-		time.Sleep(1 * time.Second)
+		sleeper.Sleep()
 	}
 	fmt.Fprint(output, finalWord)
 }
 
 func main() {
-	Countdown(os.Stdout)
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
 }
 
 // This is a nice example where using dependency ejection and mocking saves
--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -5,10 +5,19 @@ import (
 	"testing"
 )
 
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
 func TestCountdown(t *testing.T) {
 	buffer := &bytes.Buffer{}
+	spySleeper := &SpySleeper{}
 
-	Countdown(buffer)
+	Countdown(buffer, spySleeper)
 
 	got := buffer.String()
 	expected := `3
@@ -18,5 +27,9 @@ Go!`
 
 	if got != expected {
 		t.Errorf("got %q expected %q", got, expected)
+	}
+
+	if spySleeper.Calls != 3 {
+		t.Errorf("Not enough calls to sleeper. Expected 3 got %d", spySleeper.Calls)
 	}
 }

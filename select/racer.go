2adf7d893e8f90633356c5cf52931962931e6886
Victor Hugo M. Pinto <victor2142@gmail.com>
2024-07-13 12:17:49 -0300

    Refactor racer.go

--- select/racer.go
+++ select/racer.go
@@ -11,17 +11,20 @@ import (
 )
 
 func Racer(url1, url2 string) (winner string) {
-	startUrl1 := time.Now()
-	http.Get(url1)
-	url1Duration := time.Since(startUrl1)
-
-	startUrl2 := time.Now()
-	http.Get(url2)
-	url2Duration := time.Since(startUrl2)
+	url1Duration := measureResponseTime(url1)
+	url2Duration := measureResponseTime(url2)
 
 	if url1Duration < url2Duration {
 		return url1
 	}
 
 	return url2
+}
+
+func measureResponseTime(url string) time.Duration {
+	start := time.Now()
+	http.Get(url)
+	duration := time.Since(start)
+
+	return duration
 }
